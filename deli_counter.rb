70ab74f66e4990f4e6fa09c0560f68a7230fc4ce
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 16:40:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,11 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  number = 0
+  while (katz_deli.length > 0)
+    puts "Currently serving #{katz_deli[0]}"
+  end
+  puts "There is nobody waiting to be served"
+end
