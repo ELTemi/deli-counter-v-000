5a9c31b8f53f0782c992dab113f6a3a8d1dda9f0
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 16:41:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def now_serving(katz_deli)
   number = 0
   while (katz_deli.length > 0)
     puts "Currently serving #{katz_deli[0]}"
+    number -= 1
   end
   puts "There is nobody waiting to be served"
 end
