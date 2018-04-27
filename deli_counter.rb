93823481ecb11f1a022f61372e2951e47bdd43c9
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 16:21:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  i = 0
+  lines = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.map { |line| lines << "#{i + 1}. #{line}}
+
+  end
+  return "The line is currently: line.join"
+end
