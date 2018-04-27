713162c6f07d2a1ce3d9a8fb836b1342cd425173
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 16:34:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,5 +14,5 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcone #{name}, You are currently number #{katz_deli.length + 1} in line."
+  puts "Welcone #{name}. You are number #{katz_deli.length + 1} in line."
 end
