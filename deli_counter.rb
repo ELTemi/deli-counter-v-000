7971b67d26a81420e2f6ede50630a41aae4b56f9
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 16:25:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.map do |line| 
+    katz_deli.map do |list|
       lines << "#{i + 1}. #{line}
-    end 
+    end
   end
   puts "The line is currently"
 end
